749aedf35e0fdf6969d795cb0484c5eac32dc304
Mokshith <mokshith.1999@gmail.com>
2020-10-12 12:25:48 +0530

    added add product functionalit with mongo

--- util/database.js
+++ util/database.js
@@ -1,8 +1,26 @@
-const Sequelize = require('sequelize');
+const MongoClient=require("mongodb").MongoClient
+const uri="mongodb+srv://new_user:Mokshith@007@cluster0.inswv.mongodb.net/shop?retryWrites=true&w=majority"
+const mongo=new MongoClient(uri,{ useNewUrlParser: true, useUnifiedTopology: true });
 
-const sequelize = new Sequelize('node_complete', 'root', 'Mokshith@007', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect=(cb)=>{
+  mongo.connect()
+  .then(client=>{
+    console.log('Connected')
+    _db=client.db();
+    cb()
+  })
+  .catch(err=>{
+    console.log(err);
+    throw err;
+  })
+}
+const getDb=()=>{
+  if(_db){
+    return _db;
+  }
+  throw 'No database found!'
+}
+exports.mongoConnect=mongoConnect
+exports.getDb=getDb
