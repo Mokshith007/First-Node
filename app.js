9503bd5bbc994ff7e26d0a70e433614b54b42a54
Mokshith <mokshith.1999@gmail.com>
2020-10-15 20:11:53 +0530

    final mongodb

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const mongoConnect =require("./util/database").mongoConnect
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -11,19 +14,24 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const errorController=require("./controllers/error")
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req,res,next)=>{
- next();
-})
+app.use((req, res, next) => {
+  User.findById('5f87ef6a69815ad2bc1cc62b')
+    .then(user => {
+      req.user = new User(user.name,user.email,user.cart,user._id);
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-
 app.use(errorController.get404);
 
-mongoConnect(()=>{
+mongoConnect(() => {
   app.listen(3000);
-})
+});
--- controllers/shop.js
+++ controllers/shop.js
@@ -42,10 +42,7 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user.getCart()
-  .then(cart=>{
-    return cart.getProducts();
-  })
-  .then(products => {
+  .then(products=>{
     res.render('shop/cart', {
       path: '/cart',
       pageTitle: 'Your Cart',
@@ -56,44 +53,19 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  let fetchedCart
-  req.user
-  .getCart()
-  .then(cart=>{
-    fetchedCart=cart
-    return cart.getProducts({where :{id:prodId}})
+  Product.findById(prodId)
+  .then(product=>{
+    return req.user.addToCart(product)
   })
-  .then(products=>{
-    let product;
-    if(products.length>0){
-      product=products[0];
-    }
-    let  newQuantity=1;
-    if(product){
-        const oldQuantity= product.cartItem.quantity
-        newQuantity=oldQuantity+1
-        return fetchedCart.addProduct(product,{through:{quantity:newQuantity}})
-    }
-    return Product.findByPk(prodId)
-    .then(product=>{
-      return fetchedCart.addProduct(product,{through:{ quantity:newQuantity}})
-    })
-    .catch()
+  .then(result=>{
+    console.log(result);
   })
-  .catch()
   res.redirect('/cart');
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  req.user.getCart()
-  .then(cart=>{
-   return cart.getProducts({where:{id:prodId}})
-  })
-  .then(products=>{
-    const product=products[0]
-    return product.cartItem.destroy();
-  })
+  req.user.deleteItemFromCart(prodId)
   .then(result=>{
     res.redirect('/cart');
   })
@@ -101,7 +73,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user.getOrders({include:['products']})
+  req.user.getOrders()
   .then(orders=>{
     res.render('shop/orders', {
       path: '/orders',
@@ -112,26 +84,10 @@ exports.getOrders = (req, res, next) => {
 
 };
 exports.postOrder=(req,res,next)=>{
-  let fetchedCart;
-  req.user.getCart()
-  .then(cart=>{
-    fetchedCart=cart;
-    return cart.getProducts();
-  })
-  .then(products=>{
-    return req.user.createOrder().then(order=>{
-      return order.addProducts(products.map(product=>{
-        product.orderItem={quantity:product.cartItem.quantity}
-        return product;
-      }))
-    })
-    .then(result=>{
-      return fetchedCart.setProducts(null)
-    })
+ req.user.addOrder()
     .then(result=>{
       res.redirect("/orders")
     })
-  })
 }
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
