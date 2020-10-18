fdf785fb32074a6144d740ebd9ba48798fa7b8e9
Mokshith <mokshith.1999@gmail.com>
2020-10-18 13:00:37 +0530

    mongoose imple

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
+const mongoose=require('mongoose')
 
 const app = express();
 
@@ -19,9 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5f87ef6a69815ad2bc1cc62b')
+  User.findById('5f8bea90983066489c012289')
     .then(user => {
-      req.user = new User(user.name,user.email,user.cart,user._id);
+      req.user = user;
       next();
     })
     .catch(err => console.log(err));
@@ -32,6 +32,24 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+mongoose.connect('mongodb+srv://new_user:Mokshith@007@cluster0.inswv.mongodb.net/shop?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true})
+.then(result=>{
+  User.findOne().then(
+    user=>{
+      if(!user){
+        const user=new User({
+          name:'Mokshith',
+          email:'Mokshith.1999@gmail.com',
+          cart:{
+            items:[]
+          }
+        })
+        user.save()
+      }
+    }
+  )
+  
   app.listen(3000);
-});
+}).catch(err=>{
+  console.log(err)
+})
