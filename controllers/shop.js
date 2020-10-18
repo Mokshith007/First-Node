cf6a1d019d9b166b5731dd70fbebc7dec21dfe8c
Mokshith <mokshith.1999@gmail.com>
2020-10-18 19:38:58 +0530

    cart functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -41,8 +41,11 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  req.user.getCart()
-  .then(products=>{
+  req.user
+  .populate('cart.items.productId')
+  .execPopulate()
+  .then(user=>{
+    const products=user.cart.items
     res.render('shop/cart', {
       path: '/cart',
       pageTitle: 'Your Cart',
@@ -65,7 +68,7 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  req.user.deleteItemFromCart(prodId)
+  req.user.removeFromCart(prodId)
   .then(result=>{
     res.redirect('/cart');
   })
