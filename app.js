4dd4eea9ca7f62493fddf28aa4b45f61aff8fb2e
Mokshith <mokshith.1999@gmail.com>
2020-10-19 20:09:19 +0530

    added session and cookies

--- app.js
+++ app.js
@@ -6,33 +6,47 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 const mongoose=require('mongoose')
-
+const session=require('express-session')
+const MongoDbStore=require('connect-mongodb-session')(session);
+const MONGODB_URI='mongodb+srv://new_user:Mokshith@007@cluster0.inswv.mongodb.net/shop?retryWrites=true&w=majority'
 const app = express();
-
+const store=new MongoDbStore({
+  uri:MONGODB_URI,
+  collection:'session',
+})
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
+const authRoutes=require("./routes/auth");
+const { collection } = require('./models/user');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('5f8bea90983066489c012289')
+app.use(session({
+    secret:'my secret',
+    resave:false,
+    saveUninitialized:false,
+    store:store
+}))      
+app.use((req,res,next)=>{
+  if(!req.session.user){
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
-      req.user = user;
-      next();
+      req.user=user
+      next()
     })
     .catch(err => console.log(err));
-});
-
+})
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
+app.use(authRoutes)
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://new_user:Mokshith@007@cluster0.inswv.mongodb.net/shop?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true})
 .then(result=>{
   User.findOne().then(
     user=>{
